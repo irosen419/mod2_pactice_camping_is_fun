7f59bfe007a998f40b8280b3543ef84740b0a15e
irosen419 <irosen419@gmail.com>
2020-08-11 22:26:59 -0400

    completed lab

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -1,2 +1,10 @@
 class Camper < ApplicationRecord
+    has_many :signups
+    has_many :activities, through: :signups
+    validates :name, uniqueness: true
+    validates :age, inclusion: 8..18
+
+    def name_plus_age
+        "#{self.name} --- #{self.age}"
+    end
 end
